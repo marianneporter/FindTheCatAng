71f247be9a4e86e828981e840ff71da08072b7e1
marianneporter <mariannneporter17@gmail.com>
2021-12-15 21:24:35 +0000

    added demo banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 import { AppStateService } from './_services/app-state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: ([
+    trigger('enable', [
+        state('enabled', style ({
+            opacity: '1'  
+        })),
+        state('disabled', style ({          
+            opacity: '0.5',    
+        })), 
+        transition('disabled => enabled', [
+            animate('2s 1s ease')
+        ])  
+    ]),  
+  ])
 })
+
 export class AppComponent {
-    title = 'FindTheCat';
+    title = 'FindTheCat';    
        
     get enable(): string {
         return this.appState.displayWarning ? 'disabled' : 'enabled'; 
     }
 
-    constructor(private appState: AppStateService) { }
+    constructor(private appState: AppStateService) { 
+        console.log('displayWarning is ' + this.appState.displayWarning);
+    }
 
 }
